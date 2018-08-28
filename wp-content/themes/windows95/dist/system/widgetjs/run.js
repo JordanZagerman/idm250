b5b43ebdd2cad9aaa4158bdc365109171b511cfc
Jordan Zagerman <jordan.zagerman@drexel.edu>
2018-08-28 18:27:50 -0400

    javascript path partially fixed

--- wp-content/themes/windows95/dist/system/widgetjs/run.js
+++ wp-content/themes/windows95/dist/system/widgetjs/run.js
@@ -4,9 +4,10 @@
 // we should eventually replace the hardcoded aliases here with a database-type system
 // loaded dynamically
 function aliasRun(rawCommand) {
+	var themeUri = $('[data-theme-uri]').attr('data-theme-uri');
 	var command = rawCommand.toLowerCase(); // for case insensitivity
 	if(command == "debug") {
-		var debugJsLink = $("<script src='system/debug/debug.js'>");
+		var debugJsLink = $("<script src='"+themeUri+"system/debug/debug.js'>");
 		$("head").append(debugJsLink);
 		winDebug();
 	} else if (command == "winver") {
@@ -14,6 +15,6 @@ function aliasRun(rawCommand) {
 	} else {
 
 		// put file not found error dialog js here
-		makeWindow('system/icons/find24.png', 'Find', 'pages/search_results_page.html', true);
+		makeWindow(themeUri + 'system/icons/find24.png', 'Find', themeUri + 'pages/search_results_page.html', true);
 	}
 }
--- wp-content/themes/windows95/dist/system/widgets.js
+++ wp-content/themes/windows95/dist/system/widgets.js
@@ -163,6 +163,28 @@ function run(start) { // the run box
 	});
 }
 
+/* RUN DIALOG BACKEND JS - specify run box aliases and the functions they execute */
+/*      this file gets loaded dynamically whenever the run dialog is opened      */
+
+// we should eventually replace the hardcoded aliases here with a database-type system
+// loaded dynamically
+function aliasRun(rawCommand) {
+	var themeUri = $('[data-theme-uri]').attr('data-theme-uri');
+
+	var command = rawCommand.toLowerCase(); // for case insensitivity
+	if(command == "debug") {
+		var debugJsLink = $("<script src='"+themeUri+"system/debug/debug.js'>");
+		$("head").append(debugJsLink);
+		winDebug();
+	} else if (command == "winver") {
+		winverStart();
+	} else {
+
+		// put file not found error dialog js here
+		makeWindow(themeUri + 'system/icons/find24.png', 'Find', themeUri + 'pages/search_results_page.html', true);
+	}
+}
+
 function runOK() {
 	var boxVal = $("#win_wid_run_input").val();
 	aliasRun(boxVal); // sends the input to the alias => JS function (run.js)
