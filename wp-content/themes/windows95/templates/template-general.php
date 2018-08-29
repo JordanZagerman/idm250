1e6f7765e4fbe04f9208847010148761ae4dc286
Jordan Zagerman <jordan.zagerman@drexel.edu>
2018-08-29 19:24:24 -0400

    a couple pages done, need shake shack

--- wp-content/themes/windows95/templates/template-general.php
+++ wp-content/themes/windows95/templates/template-general.php
@@ -5,7 +5,7 @@
           <article class="post">   
             <?php // TO SHOW THE PAGE CONTENTS?>
             <?php while (have_posts()) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
-              <?php the_content(); ?> <!-- Page Content (WYSIWYG Field) -->
+              <p class="about_text"><?php the_content(); ?><p> <!-- Page Content (WYSIWYG Field) -->
             <?php endwhile; //resetting the page loop?>
             <?php wp_reset_query(); //resetting the page query?>
           </article>
--- wp-content/themes/windows95/templates/template-pages.php
+++ wp-content/themes/windows95/templates/template-pages.php
@@ -3,24 +3,32 @@
 Template Name: Window Page
 */
 get_header('pages');
-
-
 ?>
 
-    <h1 class="page_heading_1"><?php the_title();?></h1>
+
+<?php get_field('page_header'); ?>
+    <h1 class="page_heading_1"><?php the_field('page_header'); ?></h1>
     <div id="widget_cont">
         <!-- start widget html -->
         <div class="win_wid_NAME_tgt"></div>
         <div id="win_wid_NAME_container">
-            <img src="<?php the_post_thumbnail_url();?>" alt="About Picture" class="about_picture">
-            
+            <!--  -->
+       
 
 <?php if (have_posts()): ?>
 
           <article class="post">   
             <?php // TO SHOW THE PAGE CONTENTS?>
             <?php while (have_posts()) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
-              <?php the_content(); ?> <!-- Page Content (WYSIWYG Field) -->
+              <?php if ( has_post_thumbnail() ) { ?>
+
+                <img src="<?php the_post_thumbnail_url();?>" alt="About Picture" class="about_picture">
+             
+             <?php  } 
+                the_content();
+            
+
+              ?> <!-- Page Content (WYSIWYG Field) -->
             <?php endwhile; //resetting the page loop?>
             <?php wp_reset_query(); //resetting the page query?>
           </article>
