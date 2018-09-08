dcd677035b5bcd77658e86d052e20b8a2fd65e29
Jordan Zagerman <jordan.zagerman@drexel.edu>
2018-09-07 20:05:22 -0400

    fixed max/min bug

--- wp-content/themes/windows95/dist/system/win95.js
+++ wp-content/themes/windows95/dist/system/win95.js
@@ -60,10 +60,10 @@ $(document).ready(function()
   // MINIMIZE BUTTON
   $("#win_container").on('mousedown', '.win_titb_min', function()
   {
-    $(this).attr("src", $('[data-theme-uri]').attr('data-theme-uri') + themeUri + "system/win_controls/min_dep.png");
+    $(this).attr("src", themeUri + "system/win_controls/min_dep.png");
     $(this).mouseup(function()
     {
-      $(this).attr("src", $('[data-theme-uri]').attr('data-theme-uri') + themeUri + "system/win_controls/min.png");
+      $(this).attr("src", themeUri + "system/win_controls/min.png");
       var windc_id = $(this).parent().attr("id");
       var windc_id_splt = windc_id.split("_");
       var win_id = windc_id_splt[2];
@@ -75,13 +75,12 @@ $(document).ready(function()
   });
   // MAXIMIZE BUTTON
     // Make Window appear fullscreen on mobile when clicked
-    
     $("#win_container").on('mousedown', '.win_titb_max', function()
     {
-      $(this).attr("src", $('[data-theme-uri]').attr('data-theme-uri') + themeUri + "system/win_controls/max_dep.png");
+      $(this).attr("src", themeUri + "system/win_controls/max_dep.png");
       $(this).mouseup(function()
       {
-        $(this).attr("src", $('[data-theme-uri]').attr('data-theme-uri') + themeUri + "system/win_controls/max.png");
+        $(this).attr("src", themeUri + "system/win_controls/max.png");
         var windc_id = $(this).parent().attr("id");
         var windc_id_splt = windc_id.split("_");
         var win_id = windc_id_splt[2];
@@ -194,49 +193,6 @@ $(document).ready(function()
     });
   });
 
-  // dragElement(document.getElementById("win_window win_drag win_window_active"));
-
-  // function dragElement(elmnt) {
-  //   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  //   if (document.getElementById(elmnt.id + "header")) {
-  // 	/* if present, the header is where you move the DIV from:*/
-  // 	document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  //   } else {
-  // 	/* otherwise, move the DIV from anywhere inside the DIV:*/
-  // 	elmnt.onmousedown = dragMouseDown;
-  //   }
-
-  //   function dragMouseDown(e) {
-  // 	e = e || window.event;
-  // 	e.preventDefault();
-  // 	// get the mouse cursor position at startup:
-  // 	pos3 = e.clientX;
-  // 	pos4 = e.clientY;
-  // 	document.onmouseup = closeDragElement;
-  // 	// call a function whenever the cursor moves:
-  // 	document.onmousemove = elementDrag;
-  //   }
-
-  //   function elementDrag(e) {
-  // 	e = e || window.event;
-  // 	e.preventDefault();
-  // 	// calculate the new cursor position:
-  // 	pos1 = pos3 - e.clientX;
-  // 	pos2 = pos4 - e.clientY;
-  // 	pos3 = e.clientX;
-  // 	pos4 = e.clientY;
-  // 	// set the element's new position:
-  // 	elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-  // 	elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  //   }
-
-  //   function closeDragElement() {
-  // 	/* stop moving when mouse button is released:*/
-  // 	document.onmouseup = null;
-  // 	document.onmousemove = null;
-  //   }
-  // }
-
 
   // WINDOW RESIZE FUNCTION
   $("#win_container").on('mouseover', function()
@@ -291,7 +247,7 @@ function makeWindow(icon, title, frameurl, taskbar, res, wid, hei)
   // determine Window control type
   if (res != false && taskbar != false)
   { // all buttons
-    var sec7 = '"><img src=" ' + themeUri + 'system/win_controls/min.png" class="win_titb_min" id"_"><img src="' + themeUri + 'system/win_controls/max.png" class="win_titb_max" id"_"><img src="' + themeUri + 'system/win_controls/close.png" class="win_titb_close" id"_"></div></div></div>';
+    var sec7 = '"><img src="' + themeUri + 'system/win_controls/min.png" class="win_titb_min" id"_"><img src="' + themeUri + 'system/win_controls/max.png" class="win_titb_max" id"_"><img src="' + themeUri + 'system/win_controls/close.png" class="win_titb_close" id"_"></div></div></div>';
   }
   else if (res == false && taskbar == false)
   { // only close
